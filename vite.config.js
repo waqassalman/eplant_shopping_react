353d2b9d616c4f2f27477a9c74c65eda06a4a971
waqassalman <waqasworkid@gmail.com>
2025-09-05 21:29:52 -0400

    Cart functionality added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/plant-shopping-react-app",
   plugins: [react()],
 })
