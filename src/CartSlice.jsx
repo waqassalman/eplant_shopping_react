84179fd6a9a4f1f769bebf77420bdb56dfce6c46
waqassalman <waqasworkid@gmail.com>
2025-09-06 08:31:40 -0400

    Cart done

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -20,6 +20,7 @@ export const CartSlice = createSlice({
 },
     removeItem: (state, action) => {
         state.items = state.items.filter(item => item.name !== action.payload);
+
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
