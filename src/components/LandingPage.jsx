cbaa5cfc040fd5b64810f7416cc60b51fb1acf07
unknown <waqasworkid@gmail.com>
2025-09-12 16:46:29 -0400

    Changes added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -24,7 +24,7 @@ function LandingPage() {
           <div className="column home-image-col">
             <img
               className="home-image"
-              src='./src/assets/plant-2.png'
+              src="/eplant_shopping_react/src/assets/plant-2.png"
               alt=""
             />
           </div>
